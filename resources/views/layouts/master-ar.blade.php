4f4b9a9aabd6884118f9b1de07b7f4a124b38e03
taherm <mukadam.taher@gmail.com>
2018-04-16 09:29:08 +0300

    authentication

--- resources/views/layouts/master-ar.blade.php
+++ resources/views/layouts/master-ar.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Instructor</title>
+  <title>MubarakEsmail</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
